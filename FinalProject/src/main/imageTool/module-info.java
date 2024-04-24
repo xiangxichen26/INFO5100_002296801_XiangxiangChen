149421e5edffe1dc39b01977ba56b338c6065007
xiangxichen26 <chen.xiangxi@northeastern.edu>
2024-04-24 13:15:38 -0700

    delete useless files

--- FinalProject/src/main/imageTool/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.finalproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.finalproject to javafx.fxml;
-    exports org.example.finalproject;
-}
